d3276d45f95d972330a57badcec56a6f27aa6300
Leo Zwindrecht <leozwindrecht@outlook.com>
2026-04-11 17:20:36 +0200

    Added todo of mutations / research

--- deepMutate/Models/Functions/MutateFuncs.cs
+++ deepMutate/Models/Functions/MutateFuncs.cs
@@ -8,6 +8,31 @@ namespace deepMutate.Models.Data
 {
     public class MutateFuncs
     {
-        
+        /* TODO
+        1. Point mutations: 
+                                GCA to 
+                                GCC
+
+        2. Indels
+        Frameshift mutations:   ATG-CGT-ACG to 
+                                ATC-GTA-CG
+
+        In frame insert / delete:   ATG-CGT-ACG to
+                                    ATG-GGC-CGT-ACG 
+
+        3. Structural chromeosome mutations
+        Duplications:   ABCDEF to 
+                        ABCDEFDEF
+
+        Deletion:       ABCDEF to
+                        ABEF
+
+        Inversion:      ABCDEF to
+                        ABFEDC  
+        Translokation:  ABCDEF is translocated to a completely
+                        different part
+    
+        Evolutions: How often are the mutations applied
+        */
     }
 }
