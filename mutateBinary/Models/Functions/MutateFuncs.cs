981179557505210a9d660f0eb066b359622633f9
Leo Zwindrecht <leozwindrecht@outlook.com>
2026-04-21 23:27:25 +0200

    UI Works Slider added.

--- mutateBinary/Models/Functions/MutateFuncs.cs
+++ mutateBinary/Models/Functions/MutateFuncs.cs
@@ -9,25 +9,25 @@ namespace mutateBinary.Models.Data
     public class MutateFuncs
     {
         /* TODO
-        1. Point mutations: 
+        A 1. Point mutations: 
                                 GCA to 
                                 GCC
 
-        2. Indels
+        B 2. Indels
         Frameshift mutations:   ATG-CGT-ACG to 
                                 ATC-GTA-CG
 
-        In frame insert / delete:   ATG-CGT-ACG to
+        C In frame insert / delete:   ATG-CGT-ACG to
                                     ATG-GGC-CGT-ACG 
 
         3. Structural chromeosome mutations
-        Duplications:   ABCDEF to 
+        D Duplications:   ABCDEF to 
                         ABCDEFDEF
 
-        Deletion:       ABCDEF to
+        E Deletion:       ABCDEF to
                         ABEF
 
-        Inversion:      ABCDEF to
+        F Inversion:      ABCDEF to
                         ABFEDC  
         Translokation:  ABCDEF is translocated to a completely
                         different part
